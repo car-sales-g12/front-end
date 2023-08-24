d2faed57143023487b2c973ca0748c5539c15108
gabriel <gabriel.c8944@gmail.com>
2023-08-24 10:27:51 -0300

    fix bugs of imports

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,6 +1,8 @@
 
 import { StyledMain } from "./style";
-
+import { Header } from "../../components/Header";
+import CarList from "../../components/CarList";
+import { Footer } from "../../components/Footer";
 export const HomePage = () => {
     return (
         <>
